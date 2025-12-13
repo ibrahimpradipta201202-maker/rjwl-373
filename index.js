879b928d47deb6591de975156dbae4719e8f5421
rjwl-owner <ibrahimpradipta201202@gmail.com>
2025-12-13 21:59:34 +0700

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,9 +0,0 @@
-import { getDatabase, ref, push } from
-"https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
-
-const db = getDatabase(app);
-
-push(ref(db, "TES"), {
-  cek: "masuk",
-  waktu: Date.now()
-});
